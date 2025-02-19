335752ea3fc376947816d6f8963cb315ede10a0b
jlaron230 <jlaron230@hotmail.fr>
2025-02-19 10:40:17 +0100

    Add/Routage

---
+++ src/pages/Subscribe.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SubscribeForm from "../components/login/SubscribeForm";
+
+export default function Subscribe () {
+    return (
+        <>
+        <SubscribeForm />
+        </>
+    )
+}
